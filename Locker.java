7adc61dc3d45b8ee63a60f19efd1635457e7db11
nikiAbramov <48668280+nikiAbramov@users.noreply.github.com>
2019-04-12 21:09:47 +0300

    added test and some implementations

--- Locker.java
+++ Locker.java
@@ -8,16 +8,29 @@ public class Locker {
     private Integer capacity;
     private HashMap<String, Integer> items;
 
+    /**
+     * This is the constructor. it creates a new object of Locker.
+     * @param capacity the ammount of storge units the locker has for storing items
+     */
     public Locker(int capacity){
         this.capacity = capacity;
         this.items = new HashMap<String, Integer>();
     }
 
+    /**
+     * This method adds an items to the locker. it can be successful or not and according to the result it will return
+     * an appropriate int value
+     * @param item the item we want to put into the locker
+     * @param n the ammount of said item we want to put
+     * @return 0 if the items were added to the locker
+     *          -1 if the items weren't added
+     *          1 if the items were added, but it caused some items in the locker to move to long-term storage
+     */
     public int addItem(Item item, int n){
         if (items.containsKey(item.getType())){
-            int currentInventory = items.get(item.getType());
-            currentInventory += n;
-            items.put(item.getType(), currentInventory);
+            int currentquantityOfItem = items.get(item.getType());
+            currentquantityOfItem += n;
+            items.put(item.getType(), currentquantityOfItem);
         }
         else{
             items.put(item.getType(), n);
@@ -25,14 +38,52 @@ public class Locker {
         return 0;
     }
 
+    public int removeItem(Item item, int n){
+        return -1;
+    }
+
+    public int getItemCount(String type){
+        return -1;
+    }
+
+    /**
+     * This method returns a map of all item types with there respective quanteties in the locker
+     * @return an item of item types and their amount in the locker
+     */
+    public Map<String, Integer> getInventory(){
+        return this.items;
+    }
+
+    /**
+     * This method returns the locker's capacity
+     * @return int of the lockers capacity
+     */
     public int getCapacity() { return this.capacity; }
 
+    /**
+     * This method returns the amount of space units which are unoccupied by items
+     * @return int of the available space
+     */
     public int getAvailableCapacity(){
         int availableCapacity = this.capacity;
         Iterator iter = items.entrySet().iterator();
         while(iter.hasNext()){
             Map.Entry pair = (Map.Entry)iter.next();
-            availableCapacity -= (int)pair.getValue() * 2;
+            int itemVolume = 0;
+            switch (pair.getKey().toString()){
+                case "baseball bat":
+                    itemVolume = 2;
+                    break;
+                case "helmet, size 1":
+                    itemVolume = 3;
+                    break;
+                case "helmet, size 3":
+                    itemVolume = 5;
+                    break;
+                case "spores engine":
+                    itemVolume = 10;
+            }
+            availableCapacity -= (int)pair.getValue() * itemVolume;
         }
         return availableCapacity;
     }
--- LockerTest.java
+++ LockerTest.java
@@ -7,7 +7,7 @@ public class LockerTest {
     private Locker testLocker;
     private static ItemFactory itemFactory;
 
-    final int TEST_LOCKER_SIZE_1 = 20;
+    final int TEST_LOCKER_SIZE_DEFAULT = 100;
 
     @BeforeClass
     public static void createItemFactory(){
@@ -16,21 +16,31 @@ public class LockerTest {
 
     @Before
     public void createTestLocker(){
-        testLocker = new Locker(TEST_LOCKER_SIZE_1);
-        Item item = itemFactory.createSingleItem("baseball bat");
-        testLocker.addItem(item, 2);
+        testLocker = new Locker(TEST_LOCKER_SIZE_DEFAULT);
     }
 
     @Test
     public void testGetCapacity(){
-        Assert.assertEquals("didn't get expected capacity",TEST_LOCKER_SIZE_1, testLocker.getCapacity());
+        Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT, testLocker.getCapacity());
+        testLocker = new Locker(100);
+        Assert.assertEquals(100, testLocker.getCapacity());
+        testLocker = new Locker(0);
+        Assert.assertEquals(0, testLocker.getCapacity());
     }
 
     @Test
     public void testGetAvailableCapacity(){
-        Assert.assertEquals(TEST_LOCKER_SIZE_1 - 4, testLocker.getAvailableCapacity());
+        //Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT, testLocker.getAvailableCapacity());
         testLocker.addItem(itemFactory.createSingleItem("baseball bat"), 2);
-        Assert.assertEquals(TEST_LOCKER_SIZE_1 - 8, testLocker.getAvailableCapacity());
+        //Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT - 4, testLocker.getAvailableCapacity());
+        testLocker.addItem(itemFactory.createSingleItem("helmet, size 1"), 1);
+        Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT - 7, testLocker.getAvailableCapacity());
+        testLocker.addItem(itemFactory.createSingleItem("helmet, size 3"), 2);
+        Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT - 17, testLocker.getAvailableCapacity());
+        testLocker.addItem(itemFactory.createSingleItem("spores engine"), 2);
+        Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT - 37, testLocker.getAvailableCapacity());
+        testLocker.removeItem(itemFactory.createSingleItem("baseball bat"), 1);
+        Assert.assertEquals(TEST_LOCKER_SIZE_DEFAULT - 35, testLocker.getAvailableCapacity());
     }
 
     @Ignore
